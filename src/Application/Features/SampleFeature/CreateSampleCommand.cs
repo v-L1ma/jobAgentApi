48d158c942e7ac6e12eae8cfa45a48af406e964c
Vinicius Lima <102876820+v-L1ma@users.noreply.github.com>
2026-04-08 13:48:10 -0300

    fix: removendo feature de exemplo

--- src/Application/Features/SampleFeature/CreateSampleCommand.cs
+++
@@ -1,6 +0,0 @@
-using jobAgentApi.Application.Abstractions.Messaging;
-
-namespace jobAgentApi.Application.Features.SampleFeature
-{
-    public record CreateSampleCommand(string Name) : ICommand<int>;
-}
